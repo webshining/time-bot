5ad8ae372025fe31b86151c3607cf6c75797e838
webshining <webshinings@gmail.com>
2022-11-14 23:40:07 +0200

    fix main file

--- main.py
+++ main.py
@@ -7,18 +7,17 @@ from src.generate_img import generate_img
 
 app = Client('timebot', config('API_ID', cast=int), config('API_HASH', cast=str))
 
+
 async def main():
-    while True:
-        path, time_to_sleep = generate_img()
-        async with app:
+    async with app:
+        while True:
+            path, time_to_sleep = generate_img()
             photos = [p async for p in app.get_chat_photos((await app.get_me()).id)]
             if photos:
                 await app.delete_profile_photos(photos[0].file_id)
             await app.set_profile_photo(photo=path)
-        await asyncio.sleep(time_to_sleep)
-    
+            await asyncio.sleep(time_to_sleep)
+
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    t1 = loop.create_task(main())
-    loop.run_until_complete(t1)
+    app.run(main())
