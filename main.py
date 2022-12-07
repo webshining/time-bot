bd0aacf4a66c96fdc557421c8048406324da4660
webshining <webshinings@gmail.com>
2022-12-07 20:26:39 +0200

    added the ability to insert text on the GIF

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ async def main():
     async with app:
         while True:
             text = get_current_time().strftime("%H:%M")
-            path = generate_img(text)
+            path = generate_img(text, 'img.gif', 'RubikBurned-Regular.ttf', 100, (500, 500), 'profile.gif')
             photos = [p async for p in app.get_chat_photos((await app.get_me()).id)]
             await app.set_profile_photo(photo=path)
             if photos:
--- src/generate_img.py
+++ src/generate_img.py
@@ -1,35 +1,42 @@
-from PIL import ImageDraw, ImageFont, Image
-from decouple import config, Csv
+import io
+from PIL import ImageDraw, ImageFont, Image, ImageSequence
 
 from .helper import DIR
 
 
-W, H = config('SIZE', cast=Csv(int), default=[1000, 1000])
-FONT_SIZE = config('FONT_SIZE', cast=int, default=250)
-
-def generate_img(text: str):
-    # open image, convert to RGB and crop to W, H size
-    img = Image.open(f'{DIR}/images/img.jpg').convert('RGB')
-    w, h = img.size
-    left, top = ((w - W) / 2, (h - H) / 2)
-    img = img.crop((left, top, left+W, top+H))
+def generate_img(text: str, image: str, font: str, font_size: int, img_size: tuple[int], save: str):
+    W, H = img_size
+    gif = image.split('.')[-1] == 'gif'
+    
+    # open image
+    img = Image.open(f'{DIR}/images/{image}')
+    if not gif:
+        img.convert('RGB')
+        left, top = ((img.size[0] - W) / 2, (img.size[1] - H) / 2)
+        img = img.crop((left, top, left+W, top+H))
     
     # add font to text and get it size
-    font_rubik = ImageFont.truetype(f'{DIR}/fonts/RubikBurned-Regular.ttf', FONT_SIZE)
-    w1, h1 = font_rubik.getsize(text)
-    font_covered = ImageFont.truetype(f'{DIR}/fonts/CoveredByYourGrace-Regular.ttf', 50)
-    w2, h2 = font_covered.getsize("How good when not negative good")
-    
+    font = ImageFont.truetype(f'{DIR}/fonts/{font}', font_size)
+    w, h = font.getsize(text)
     
     # draw text
-    draw = ImageDraw.Draw(img)
-    draw.text(((W-w1)/2, (H-h1)/2), text, font=font_rubik, fill="#ffffff")
-    draw.text(((W-w2-20), (H-h2-10)), text="How good when not negative good", font=font_covered, fill="#ffffff")
+    if not gif:
+        draw = ImageDraw.Draw(img)
+        draw.text(((W-w)/2, (H-h)/2), text, font=font, fill="#ffffff")
+    else:
+        frames = []
+        for frame in ImageSequence.Iterator(img):
+            draw = ImageDraw.Draw(frame)
+            draw.text(((W-w)/2, (H-h)/2), text, font=font, fill="#ffffff")
+            del draw
+            b = io.BytesIO()
+            frame.save(b, format="GIF")
+            frame = Image.open(b)
+            frames.append(frame)
     
     # save image
-    path = f'{DIR}/images/profile.jpg'
-    img.save(path)
+    path = f'{DIR}/images/{save}'
+    img.save(path) if not gif else frames[0].save(path, save_all=True, append_images=frames[1:])
     
-    # return path to img and sleep time
+    # return path to img
     return path
-    
